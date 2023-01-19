a5d1eb37ea07917ea0a2ce6d58fa1114d9926369
katiexh-0751413 <81718799+katiexh-0751413@users.noreply.github.com>
2023-01-19 14:04:22 -0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,20 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-import { ICommandPalette } from '@jupyterlab/apputils';
 import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
+
 import { Widget } from '@lumino/widgets';
 
 /**
  * Initialization data for the jupyterlab_apod extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'jupyterlab_apod',
-  autoStart: true,
-  requires: [ICommandPalette],
-  const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab-apod',
   autoStart: true,
   requires: [ICommandPalette],
-
   activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
     console.log('JupyterLab extension jupyterlab_apod is activated!');
-    
+
     // Define a widget creator function,
     // then call it to make a new widget
     const newWidget = () => {
@@ -33,26 +28,21 @@ const plugin: JupyterFrontEndPlugin<void> = {
       widget.title.closable = true;
       return widget;
     }
-
     let widget = newWidget();
 
     // Add an application command
     const command: string = 'apod:open';
-
     app.commands.addCommand(command, {
       label: 'Random Astronomy Picture',
-
       execute: () => {
         // Regenerate the widget if disposed
         if (widget.isDisposed) {
           widget = newWidget();
         }
-
         if (!widget.isAttached) {
           // Attach the widget to the main work area if it's not there
           app.shell.add(widget, 'main');
         }
-
         // Activate the widget
         app.shell.activateById(widget.id);
       }
